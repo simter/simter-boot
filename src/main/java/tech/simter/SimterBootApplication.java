36a78c9f3d9bd8b7fa4f2b897222907196b10977
RJ.Hwang <rongjihuang@gmail.com>
2016-12-28 17:44:49 +0800

    init

---
+++ src/main/java/tech/simter/SimterBootApplication.java
@@ -0,0 +1,12 @@
+package tech.simter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimterBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimterBootApplication.class, args);
+	}
+}
